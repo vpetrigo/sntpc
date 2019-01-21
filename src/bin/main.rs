47ddb7ab079857399e96d575dc1cefbf43e0384b
Vladimir Petrigo <taenaru@gmail.com>
2019-01-21 10:44:06 +0300

    bin: Clean up test application

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,20 @@
 use sntp_client;
+use std::{thread, time};
+
+#[allow(dead_code)]
+const POOL_NTP_ADDR: &str = "pool.ntp.org";
+#[allow(dead_code)]
+const GOOGLE_NTP_ADDR: &str = "time.google.com";
 
 fn main() {
-    sntp_client::request("88.147.254.232", 123);
+    for _ in 0..4000 {
+        let result = sntp_client::request(POOL_NTP_ADDR, 123);
+
+        match result {
+            Ok(time) => println!("Got time: {}", time),
+            Err(err) => println!("Err: {}", err.to_string()),
+        }
+
+        thread::sleep(time::Duration::new(1, 0));
+    }
 }
