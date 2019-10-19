7c70e1aeed5a7c2f59ba43724fccc0dc18e0fc5f
Vladimir Petrigo <taenaru@gmail.com>
2019-10-19 12:08:58 +0300

    lib: Clean up

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,10 @@
 //! let result = sntpc::request("pool.ntp.org", 123);
 //!
 //! if let Ok(sntpc::NtpResult {
-//!     sec, nsec, ..
+//!     sec, nsec, roundtrip, offset
 //! }) = result {
 //!     println!("NTP server time: {}.{}", sec, nsec);
+//!     println!("Roundtrip time: {}, offset: {}", roundtrip, offset);
 //! }
 //! ```
 
