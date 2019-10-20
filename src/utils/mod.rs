2f953bc1d048a1ce36817785ba496044044602ff
Vladimir Petrigo <taenaru@gmail.com>
2019-10-20 10:26:59 +0300

    utils: Declare sync system time interface

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,81 +1,36 @@
+use chrono::{Local, TimeZone, Timelike, Utc};
+use log::debug;
+
+#[cfg(unix)]
+use unix::sync_time;
 #[cfg(windows)]
-mod windows;
-#[cfg(windows)]
-pub use windows::*;
+use windows::sync_time;
 
 #[cfg(unix)]
 mod unix;
-#[cfg(unix)]
-pub use unix::*;
+#[cfg(windows)]
+mod windows;
 
-struct TimeZone {
-    hours: i32,
-    minutes: i32,
-    seconds: i32,
-}
+/// Set up system time based on the given parameters
+/// Args:
+/// * sec - Seconds since UNIX epoch start
+/// * nsec - Fraction of seconds from an NTP response
+pub fn update_system_time(sec: u32, nsec: u32) {
+    let time = Utc.timestamp(sec as i64, nsec);
+    let local_time = time.with_timezone(&Local);
+    debug!(
+        "UTC time: {:02}:{:02}:{:02}",
+        time.hour(),
+        time.minute(),
+        time.second()
+    );
+    debug!(
+        "{} time: {:02}:{:02}:{:02}",
+        local_time.offset(),
+        local_time.hour(),
+        local_time.minute(),
+        local_time.second()
+    );
 
-impl TimeZone {
-    fn new(hours: i32, minutes: i32, seconds: i32) -> Self {
-        assert!(
-            hours >= -12 && hours <= 12,
-            "Incorrect hour TZ offset {}",
-            hours
-        );
-        assert!(
-            minutes >= 0 && minutes <= 59,
-            "Incorrect minute TZ offset {}",
-            minutes
-        );
-        assert!(
-            seconds >= 0 && seconds <= 59,
-            "Incorrect second TZ offset {}",
-            seconds
-        );
-
-        TimeZone {
-            hours,
-            minutes,
-            seconds,
-        }
-    }
-}
-
-impl From<Vec<i32>> for TimeZone {
-    fn from(val: Vec<i32>) -> Self {
-        assert_eq!(3, val.len());
-        TimeZone::new(val[0], val[1], val[2])
-    }
-}
-
-struct TimeValue {
-    hours: u32,
-    minutes: u32,
-    seconds: u32,
-}
-
-fn get_current_timezone_time(
-    timezone: &TimeZone,
-    hours: u32,
-    minutes: u32,
-    seconds: u32,
-) -> TimeValue {
-    let result: TimeValue = TimeValue {
-        hours: hours_to_timezone(hours, timezone.hours),
-        minutes: minutes_to_timezone(minutes, timezone.minutes),
-        seconds: seconds_to_timezone(seconds, timezone.seconds),
-    };
-
-    result
-}
-
-fn hours_to_timezone(hours: u32, tz_hours: i32) -> u32 {
-    (((24 + hours) as i32 + tz_hours) % 24) as u32
-}
-
-fn minutes_to_timezone(minutes: u32, tz_min: i32) -> u32 {
-    ((minutes as i32 + tz_min) % 60) as u32
-}
-
-fn seconds_to_timezone(seconds: u32, tz_sec: i32) -> u32 {
-    ((seconds as i32 + tz_sec) % 60) as u32
+    sync_time(local_time);
 }
