dea5f026092428850b598fac16a07c219b9340f2
Vladimir Petrigo <taenaru@gmail.com>
2019-10-22 19:16:11 +0300

    utils: Add sync_time() interface for UNIX

--- src/utils/unix/mod.rs
+++ src/utils/unix/mod.rs
@@ -1,3 +1,26 @@
-pub fn update_system_time(seconds: u32) {
-    unimplemented!("Linux support has not been implemented yet");
+use std::process::Command;
+
+use chrono::{DateTime, Datelike, Local, Timelike};
+
+pub(super) fn sync_time(time: DateTime<Local>) {
+    let time_str = format!(
+        "{}/{}/{} {:02}:{:02}:{:02}",
+        time.month(),
+        time.day(),
+        time.year(),
+        time.hour(),
+        time.minute(),
+        time.second()
+    );
+    let sync_cmd_status = Command::new("date")
+        .args(&["-s", time_str.as_str()])
+        .status()
+        .expect("Unable to execute date command");
+
+    if !sync_cmd_status.success() {
+        eprintln!(
+            "Date command exit status {}",
+            sync_cmd_status.code().unwrap()
+        );
+    }
 }
