5fd98bc81a4f5d5e5e488aba7c93cd89ac04b745
Vladimir Petrigo <taenaru@gmail.com>
2019-10-23 03:29:49 +0300

    timesync: Add binary

--- src/bin/timesync.rs
+++ src/bin/timesync.rs
@@ -1,9 +1,17 @@
+use simple_logger;
+
 const GOOGLE_NTP_ADDR: &str = "time.google.com";
 
 fn main() {
+    if cfg!(debug_assertions) {
+        simple_logger::init_with_level(log::Level::Trace).unwrap();
+    } else {
+        simple_logger::init_with_level(log::Level::Info).unwrap();
+    }
+
     let time = sntpc::request(GOOGLE_NTP_ADDR, 123).expect(
         format!("Unable to receive time from: {}", GOOGLE_NTP_ADDR).as_str(),
     );
 
-    sntpc::utils::update_system_time(time.sec());
+    sntpc::utils::update_system_time(time.sec(), time.nsec());
 }
