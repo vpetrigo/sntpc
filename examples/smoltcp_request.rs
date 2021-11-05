e93f892841767c9a5a445ccb5aa24bd819dd5725
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2021-11-05 03:58:01 +0300

    misc: Clean up

---
+++ examples/smoltcp_request.rs
@@ -0,0 +1,8 @@
+// use smoltcp::phy::TapInterface;
+use smoltcp::phy::Device;
+use smoltcp::socket::{UdpSocket, UdpSocketBuffer};
+
+fn main() {
+    // let interface_name = "tap0";
+    // let tuntap = TapInterface::new(interface_name);
+}
