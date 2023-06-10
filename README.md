b613a933e1343bd1b879a221288c14231316d85c
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2023-06-11 04:10:33 +0600

    misc: Clean up

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ like so:
 
 ```toml
 [dependencies]
-sntpc = "0.3.4"
+sntpc = "0.3.5"
 ```
 
 By calling the `get_time()` method and providing a proper NTP pool or server you
@@ -40,56 +40,14 @@ use sntpc::{Error, NtpContext, NtpTimestampGenerator, NtpUdpSocket, Result};
 use std::net::{SocketAddr, ToSocketAddrs, UdpSocket};
 use std::time::Duration;
 
-#[derive(Copy, Clone, Default)]
-struct StdTimestampGen {
-    duration: Duration,
-}
-
-impl NtpTimestampGenerator for StdTimestampGen {
-    fn init(&mut self) {
-        self.duration = std::time::SystemTime::now()
-            .duration_since(std::time::SystemTime::UNIX_EPOCH)
-            .unwrap();
-    }
-    fn timestamp_sec(&self) -> u64 {
-        self.duration.as_secs()
-    }
-    fn timestamp_subsec_micros(&self) -> u32 {
-        self.duration.subsec_micros()
-    }
-}
-
-#[derive(Debug)]
-struct UdpSocketWrapper(UdpSocket);
-
-impl NtpUdpSocket for UdpSocketWrapper {
-    fn send_to<T: ToSocketAddrs>(
-        &self,
-        buf: &[u8],
-        addr: T,
-    ) -> Result<usize> {
-        match self.0.send_to(buf, addr) {
-            Ok(usize) => Ok(usize),
-            Err(_) => Err(Error::Network),
-        }
-    }
-    fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr)> {
-        match self.0.recv_from(buf) {
-            Ok((size, addr)) => Ok((size, addr)),
-            Err(_) => Err(Error::Network),
-        }
-    }
-}
 fn main() {
     let socket =
         UdpSocket::bind("0.0.0.0:0").expect("Unable to create UDP socket");
     socket
        .set_read_timeout(Some(Duration::from_secs(2)))
        .expect("Unable to set UDP socket read timeout");
-    let sock_wrapper = UdpSocketWrapper(socket);
-    let ntp_context = NtpContext::new(StdTimestampGen::default());
     let result =
-        sntpc::get_time("time.google.com:123", sock_wrapper, ntp_context);
+        sntpc::get_time_sup("time.google.com:123", socket);
 
     match result {
        Ok(time) => {
--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@
 //! Put this in your `Cargo.toml`:
 //! ```cargo
 //! [dependencies]
-//! sntpc = "0.3.1"
+//! sntpc = "0.3.5"
 //! ```
 //!
 //! ## Features
@@ -25,7 +25,6 @@
 //! <div class="example-wrap" style="display:inline-block"><pre class="compile_fail" style="white-space:normal;font:inherit;">
 //!
 //! **Warning**: `utils` feature is not stable and may change in the future.
-//!
 //! </pre></div>
 //!
 //! # Details
@@ -53,75 +52,9 @@
 //! # Example
 //!
 //! ```rust
-//! use sntpc::{Error, NtpContext, NtpTimestampGenerator, NtpUdpSocket, Result};
-//! #[cfg(not(feature = "std"))]
-//! use no_std_net::{SocketAddr, ToSocketAddrs, IpAddr, Ipv4Addr};
-//! #[cfg(feature = "std")]
-//! use std::net::{SocketAddr, ToSocketAddrs, UdpSocket};
+//! # #[cfg(feature = "std")]
+//! use std::net::UdpSocket;
 //! use std::time::Duration;
-//!
-//! #[derive(Copy, Clone, Default)]
-//! struct StdTimestampGen {
-//!     duration: Duration,
-//! }
-//!
-//! impl NtpTimestampGenerator for StdTimestampGen {
-//!     fn init(&mut self) {
-//!         self.duration = std::time::SystemTime::now()
-//!             .duration_since(std::time::SystemTime::UNIX_EPOCH)
-//!             .unwrap();
-//!     }
-//!
-//!     fn timestamp_sec(&self) -> u64 {
-//!         self.duration.as_secs()
-//!     }
-//!
-//!     fn timestamp_subsec_micros(&self) -> u32 {
-//!         self.duration.subsec_micros()
-//!     }
-//! }
-//!
-//! # #[cfg(not(feature = "std"))]
-//! # #[derive(Debug)]
-//! # struct UdpSocket;
-//! # #[cfg(not(feature = "std"))]
-//! # impl UdpSocket {
-//! #     fn bind(addr: &str) -> Result<Self> {
-//! #         Ok(UdpSocket)
-//! #     }
-//! #     fn send_to<T: ToSocketAddrs>(&self, buf: &[u8], dest: T) -> Result<usize> {
-//! #         Ok(0usize)
-//! #     }
-//! #     fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr)> {
-//! #         Ok((0usize, SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0)))
-//! #     }
-//! #     fn set_read_timeout<T>(&self, _arg: T) -> Result<()> {
-//! #         Ok(())
-//! #     }
-//! # }
-//!
-//! #[derive(Debug)]
-//! struct UdpSocketWrapper(UdpSocket);
-//!
-//! impl NtpUdpSocket for UdpSocketWrapper {
-//!     fn send_to<T: ToSocketAddrs>(
-//!         &self,
-//!         buf: &[u8],
-//!         addr: T,
-//!     ) -> Result<usize> {
-//!         match self.0.send_to(buf, addr) {
-//!             Ok(usize) => Ok(usize),
-//!             Err(_) => Err(Error::Network),
-//!         }
-//!     }
-//!
-//!     fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr)> {
-//!         match self.0.recv_from(buf) {
-//!             Ok((size, addr)) => Ok((size, addr)),
-//!             Err(_) => Err(Error::Network),
-//!         }
-//!     }
-//! }
 //!
 //! fn main() {
 //!     let socket =
@@ -129,12 +62,9 @@
 //!     socket
 //!        .set_read_timeout(Some(Duration::from_secs(2)))
 //!        .expect("Unable to set UDP socket read timeout");
-//!     let sock_wrapper = UdpSocketWrapper(socket);
-//!     let ntp_context = NtpContext::new(StdTimestampGen::default());
-//!     # #[cfg(feature = "std")]
-//!     let result =
-//!         sntpc::get_time("time.google.com:123", sock_wrapper, ntp_context);
-//!     # #[cfg(feature = "std")]
+//!     # #[cfg(all(feature = "std", feature = "sup"))]
+//!     let result = sntpc::simple_get_time("time.google.com:123", socket);
+//!     # #[cfg(all(feature = "std", feature = "sup"))]
 //!     match result {
 //!        Ok(time) => {
 //!            println!("Got time: {}.{}", time.sec(), time.sec_fraction());
@@ -145,6 +75,12 @@
 //! ```
 #![cfg_attr(not(feature = "std"), no_std)]
 
+extern crate core;
+
+#[cfg(all(feature = "std", feature = "sup"))]
+mod sup;
+#[cfg(all(feature = "std", feature = "sup"))]
+pub use crate::sup::simple_get_time;
 #[cfg(feature = "utils")]
 pub mod utils;
 
