61e87b8729d0d906bc665923f6f54d8cdec8445f
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-12-25 23:30:15 +0100

    sntpc: socket: embassy: Refactor embassy socket implementation for clarity.
    Simplified type references by removing redundant module paths. This improves code readability and reduces verbosity in handling `UdpSocket` and related types.

--- sntpc/src/socket/embassy.rs
+++ sntpc/src/socket/embassy.rs
@@ -1,19 +1,20 @@
 use crate::{net::SocketAddr, Error, NtpUdpSocket, Result};
+use embassy_net::{udp::UdpSocket, IpAddress, IpEndpoint};
 
 use core::net::IpAddr;
 
-impl NtpUdpSocket for &embassy_net::udp::UdpSocket<'_> {
+impl NtpUdpSocket for UdpSocket<'_> {
     async fn send_to(&self, buf: &[u8], addr: SocketAddr) -> Result<usize> {
         // Currently smoltcp still has its own address enum
-        let endpoint = embassy_net::IpEndpoint::new(
+        let endpoint = IpEndpoint::new(
             match addr.ip() {
-                IpAddr::V4(addr) => embassy_net::IpAddress::Ipv4(addr),
-                IpAddr::V6(addr) => embassy_net::IpAddress::Ipv6(addr),
+                IpAddr::V4(addr) => IpAddress::Ipv4(addr),
+                IpAddr::V6(addr) => IpAddress::Ipv6(addr),
             },
             addr.port(),
         );
 
-        match embassy_net::udp::UdpSocket::send_to(self, buf, endpoint).await {
+        match UdpSocket::send_to(self, buf, endpoint).await {
             Ok(()) => Ok(buf.len()),
             Err(e) => {
                 #[cfg(feature = "log")]
@@ -24,17 +25,17 @@ impl NtpUdpSocket for &embassy_net::udp::UdpSocket<'_> {
     }
 
     async fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr)> {
-        let to_addr = |ep: embassy_net::IpEndpoint| {
+        let to_addr = |ep: IpEndpoint| {
             SocketAddr::new(
                 match ep.addr {
-                    embassy_net::IpAddress::Ipv4(val) => IpAddr::V4(val),
-                    embassy_net::IpAddress::Ipv6(val) => IpAddr::V6(val),
+                    IpAddress::Ipv4(val) => IpAddr::V4(val),
+                    IpAddress::Ipv6(val) => IpAddr::V6(val),
                 },
                 ep.port,
             )
         };
 
-        match embassy_net::udp::UdpSocket::recv_from(self, buf).await {
+        match UdpSocket::recv_from(self, buf).await {
             Ok((len, ep)) => Ok((len, to_addr(ep.endpoint))),
             Err(e) => {
                 #[cfg(feature = "log")]
