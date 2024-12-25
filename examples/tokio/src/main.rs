f0b5a53dd9abef83d180e007f1940dd06cd1d588
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-12-25 12:56:00 +0100

    examples: tokio: Use `tokio::time::timeout` to handle NTP request timeouts.
    This updates the Tokio example to include timeout handling for NTP requests, improving reliability in case of unresponsive servers. Unused custom socket implementation is removed, and new "tokio-socket" and "time" features are added to dependencies for compatibility.

--- examples/tokio/src/main.rs
+++ examples/tokio/src/main.rs
@@ -1,46 +1,29 @@
-use sntpc::{
-    get_time, Error, NtpContext, NtpUdpSocket, Result, StdTimestampGen,
-};
+use sntpc::{get_time, NtpContext, StdTimestampGen};
 use tokio::net::{lookup_host, UdpSocket};
+use tokio::time::timeout;
 
 use core::net::SocketAddr;
 
 const POOL_NTP_ADDR: (&str, u16) = ("pool.ntp.org", 123);
 
-#[derive(Debug)]
-struct Socket {
-    sock: UdpSocket,
-}
-
-impl NtpUdpSocket for Socket {
-    async fn send_to(&self, buf: &[u8], addr: SocketAddr) -> Result<usize> {
-        self.sock
-            .send_to(buf, addr)
-            .await
-            .map_err(|_| Error::Network)
-    }
-
-    async fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr)> {
-        self.sock.recv_from(buf).await.map_err(|_| Error::Network)
-    }
-}
-
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let sock = UdpSocket::bind("0.0.0.0:0".parse::<SocketAddr>().unwrap())
+    let socket = UdpSocket::bind("0.0.0.0:0".parse::<SocketAddr>().unwrap())
         .await
         .expect("Socket creation");
-    let socket = Socket { sock };
     let ntp_context = NtpContext::new(StdTimestampGen::default());
 
     for addr in lookup_host(POOL_NTP_ADDR)
         .await
         .expect("Unable to resolve address")
     {
-        let res = get_time(addr, &socket, ntp_context)
-            .await
-            .expect("get_time error");
+        let duration = core::time::Duration::from_secs(2);
+        let res = timeout(duration, get_time(addr, &socket, ntp_context)).await;
 
-        println!("RESULT: {res:?}");
+        match res {
+            Ok(Ok(res)) => println!("RESULT: {res:?}"),
+            Ok(Err(err)) => println!("ERROR: {err:?}"),
+            Err(_) => println!("TIMEOUT: address {addr:?}"),
+        }
     }
 }
