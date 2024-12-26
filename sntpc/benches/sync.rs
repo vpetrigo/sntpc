3165f25297a433a9e1b965a3e7528d4b3050895f
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-12-26 20:28:46 +0100

    Add benchmarks for async and sync SNTP clients
    Introduce `criterion`-based performance benchmarks for both async and sync SNTP clients. These changes include new benchmark files, additions to `Cargo.toml`, and a CI workflow step for automated benchmarking.

---
+++ sntpc/benches/sync.rs
@@ -0,0 +1,19 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use sntpc::sync::get_time;
+use sntpc::{NtpContext, StdTimestampGen};
+use std::hint::black_box;
+use std::net::{Ipv4Addr, SocketAddr, UdpSocket};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let socket =
+        UdpSocket::bind(SocketAddr::from((Ipv4Addr::UNSPECIFIED, 0))).unwrap();
+    let addr = SocketAddr::from((Ipv4Addr::LOCALHOST, 1231));
+    let context = NtpContext::new(StdTimestampGen::default());
+
+    c.bench_function("sync_sntp_client", |b| {
+        b.iter(|| black_box(get_time(addr, &socket, context)));
+    });
+}
+
+criterion_group!(sync_benches, criterion_benchmark);
+criterion_main!(sync_benches);
